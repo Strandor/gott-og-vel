a9268423b6ba65004244cfee4169d7279078314b
Steinar Þór Smári <22420881+Strandor@users.noreply.github.com>
2020-09-01 12:47:39 +0000

    Added https

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import express from "express";
+const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(80, () => {
+app.listen(443, () => {
     console.log("Listening on port 80")
 })
