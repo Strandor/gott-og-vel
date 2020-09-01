6bb13e942a71c10a93d80a55f7833db9e3f3e2b4
Steinar Þór Smári <22420881+Strandor@users.noreply.github.com>
2020-09-01 12:55:58 +0000

    Added dynamic port

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 const express = require("express");
 import {Request, Response} from "express";
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-app.listen(80, () => {
-    console.log("Listening on port 80")
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
